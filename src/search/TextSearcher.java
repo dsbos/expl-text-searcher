559d6d867defce851c4d16ca80e48f8cae639821
Daniel Barclay <dbarclay@savi.com>
2018-11-17 15:44:58 -0500

    Cleaned up TextSearcher a lot.

--- src/search/TextSearcher.java
+++ src/search/TextSearcher.java
@@ -1,9 +1,5 @@
 package search;
 
-import com.sun.corba.se.impl.orbutil.ObjectStreamClassUtil_1_3;
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.LocatorEx;
-import sun.security.provider.certpath.OCSPResponse;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,143 +11,171 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-// ~Problems:
-// - style: uses tabs instead of spaces
-// - depends on default character set
-// - doesn't close input file
-// - style: no spaces after commas
 
+// Some notes on original code:
+// - ~problems:
+//   - FileReader(File) depends on default character set [risky; unstable]
+//   - file reading doesn't close input file
+// - style:
+//   - uses tabs instead of spaces [inconsistent rendering]
+//   - no spaces after commas [less readable]
+//   - not bottom-up order (called methods are after calling methods);
+//     [inconsistent with bottom-up order of much Java (starting with imports
+//     at top)]
 
 
 public class TextSearcher {
 
-    private final String inputText;
+    /**
+      * Represents an occurrence of a word in the indexed input text.
+      */
+	private static class Occurrence {
+	    private final int occIndex;
+        private final int startChOffset;
+        private final int endChOffset;
 
-    private final List<Occurrence> occurrences = new ArrayList<>();
+        /**
+         * ...
+         * @param  occIndex      ordinal position among all word occurrences (for
+         *                          indexing into sibling occurrences to get
+         *                          context character range without re-scanning)
+         * @param  startChOffset  occurrence's starting character offset in input
+         * @param  endChOffset    occurrence's ending character offset in input
+         */
+	    public Occurrence(int occIndex, int startChOffset, int endChOffset) {
+            this.occIndex = occIndex;
+            this.startChOffset = startChOffset;
+            this.endChOffset = endChOffset;
+        }
+        public int getOccIndex()      { return occIndex; }
+        public int getStartChOffset() { return startChOffset; }
+        public int getEndChOffset()   { return endChOffset; }
+	}
 
-    private Map<String, List<Occurrence>> occurrenceListsByWord =
-            new HashMap<>();
+    private final String indexedText;
 
-    private static boolean isWordChar(char ch) {
+	// Note:  Possible optimization:  Instead of storing (ArrayList) list of N
+    //   occurrence objects, each with index and starting and ending character
+    //   offset int values, store two int arrays, one of starting character
+    //   offsets and one of ending character offsets.
+    //   - Saves space:  object overhead of N objects, plus N pointers.
+    //   - Might help speed re CPU cache (data for logically adjacent
+    //     occurrences is adjacent array elements, in nearby memory, vs. in
+    //     other Occurrence objects, likely in farther-away memory in the heap).
+    // Note:  Less likely possible space-over-speed optimization:  Store only
+    //   starting character offset, using re-parsing to find ending character
+    //   offset.  (Change word index from listing Occurrence objects (or indexes
+    //   into parallel start-and-end-offset arrays) to directly listing
+    //   starting offsets.)
+    //   - Eliminates space for occurrence objects list or offset arrays.
+    //   - Costs time to re-parse original text from starting character offset
+    //     of hit backwards to beginning of context and forwards to end of
+    //     context.
+
+
+    private final List<Occurrence> wordOccurrences;
+
+    private final Map<String, List<Occurrence>> occurrenceListsByWord;
+
+
+    /**
+     * Reports whether given character counts as part of a word.
+     */
+    private static boolean isWordChar(final char ch) {
         return (    '0' <= ch && ch <= '9')
                 || ('A' <= ch && ch <= 'Z')
                 || ('a' <= ch && ch <= 'z')
                 || '\'' == ch;
     }
 
-    private static String canonicalizeWord(String word) {
-        return word.toLowerCase();
-    }
-
-    /**
-      * Represents an occurrence of a word in the indexed input test.
-      * @param  occOffset      ordinal position among all word occurrence (for
-      *                          indexing into sibling occurrences to get
-      *                          context)
-      * @param  startChOffset  occurrence's starting character offset in input
-      * @param  endChOffset    occurrence's end character offset in inpur
-      */
-	private static class Occurrence {
-		int occOffset;
-		int startChOffset;
-		int endChOffset;
-	}
-
-
-    //??????????
-
-    //???? add list parameter, to make static?
-    private void addOccurrence(int startChOffset, int endCharOffset) {
-	    System.err.println("addOccurrence(" + startChOffset + ", " + endCharOffset + ")");
-        int occOffset = occurrences.size();
-
-        final Occurrence temp = new Occurrence();
-        temp.occOffset = occOffset;
-        temp.startChOffset = startChOffset;
-        temp.endChOffset = endCharOffset;
-
-        occurrences.add(temp);
+    private static String canonicalizeWord(final String occurrenceForm) {
+        return occurrenceForm.toLowerCase();
     }
 
 
-    private static enum CharClass { WordChar, OtherChar }
+    private static void addOccurrence(final List<Occurrence> occurrences,
+                                      final int startChOffset,
+                                      final int endCharOffset) {
+        // What new occurrence's offset in ordered collection will be once added:
+        final int occOffset = occurrences.size();
+        occurrences.add(new Occurrence(occOffset, startChOffset, endCharOffset));
+    }
+
     /** Lexing (tokenizing) state. */
     private static enum LexState { InWord, InOther }
 
-	private void/*List<Occurrence>*/ findWordOccurrences(String inputText) {
-	    int lastWordStartOffset = -1;
+	private static List<Occurrence> findWordOccurrences(final String inputText) {
+        final List<Occurrence> occurrences = new ArrayList<>();
 
-        LexState lexState = LexState.InOther;
+	    int lastWordStartOffset = -1;  // (Initial value should be unused.)
+
+        // Handle beginning of text:
+        LexState lexState = LexState.InOther;  // Start not in any word.
+
+        // Handle text:
 	    for (int cx = 0; cx < inputText.length(); cx++) {
-            char ch = inputText.charAt(cx);
-            System.err.println("@ " + cx + ": '" + ch + "'");
-            CharClass charClass =
-					isWordChar(ch) ? CharClass.WordChar : CharClass.OtherChar;
-            System.err.println("before char.: lexState = " + lexState);
+            final char ch = inputText.charAt(cx);
+            final boolean isWordChar = isWordChar(ch);
             switch (lexState) {
 				case InOther:
-					switch (charClass) {
-                        case OtherChar:  // another non-word char
-                            // None
-                            break;
-                        case WordChar:   // first character of word--word start
-                            lexState = LexState.InWord;
-                            lastWordStartOffset = cx;
-                            // None
-                            break;
+				    if (! isWordChar) {
+                        // Not in word; a(nother) non-word char.--no-op.
+                    }
+                    else {
+				        // Not in word; a word character--word start.
+                        lexState = LexState.InWord;
+                        lastWordStartOffset = cx;
                     }
                     break;
                 case InWord:
-                    switch (charClass) {
-                        case WordChar:  // another word char
-                            //  None
-                            break;
-                        case OtherChar: // non-word char after word--word end
-                            lexState = LexState.InOther;
-     	                    addOccurrence(lastWordStartOffset, cx);
-     	                    break;
+                    if (isWordChar) {
+                        // In word; another word char.--no-op.
                     }
+                    else {
+                        // In word; a non-word char.--word end.
+                        lexState = LexState.InOther;
+                        addOccurrence(occurrences, lastWordStartOffset, cx);
+                    }
+                    break;
             }
         }
-        System.err.println("after char.: lexState = " + lexState);
+        // Handle end of text:
         switch (lexState) {
             case InOther:
+                // Ended not in word--nothing to wrap up/
                 break;
-            case InWord:  // not-yet-terminated word--word end
+            case InWord:
+                // Ended in word (not yet terminated)--word end.
                 lexState = LexState.InOther;  // (not really needed)
-              addOccurrence(lastWordStartOffset, -1 + inputText.length());
+              addOccurrence(occurrences,
+                            lastWordStartOffset, inputText.length());
               break;
         }
+        return Collections.unmodifiableList(occurrences);
     }
 
-    private void indexOccurrences(String inputText) {
+    private static Map<String, List<Occurrence>> indexOccurrences(
+            String inputText,
+            List<Occurrence> wordOccurrences) {
 	    //?? Maybe optimize:  index each occurrence as we find occurrences, when
         //   characters we just scanned, and Occurrences we created, are still
-        //   in CPUcache
+        //   in CPU cache
 
+        final Map<String, List<Occurrence>> occurrencesByWord = new HashMap<>();
 
-        occurrences.forEach(occurrence -> {
-            String occurrenceString =
-                    inputText.substring(occurrence.startChOffset,
-                                        occurrence.endChOffset);
-            System.err.println("indexOccurrences(...): occurrenceString = '" + occurrenceString + "'");
-            String canonicalString = canonicalizeWord(occurrenceString);
+        wordOccurrences.forEach(occurrence -> {
+            String occurrenceForm =
+                    inputText.substring(occurrence.getStartChOffset(),
+                                        occurrence.getEndChOffset());
+            String canonicalForm = canonicalizeWord(occurrenceForm);
             List<Occurrence> currentList =
-                    occurrenceListsByWord.computeIfAbsent(canonicalString,
-                                                          key -> new ArrayList<>());
+                    occurrencesByWord.computeIfAbsent(canonicalForm,
+                                                      word -> new ArrayList<>());
             currentList.add(occurrence);
         });
+        return Collections.unmodifiableMap(occurrencesByWord);
     }
 
-
-    /**
-   	 *  Initializes any internal data structures that are needed for
-   	 *  this class to implement search efficiently.
-   	 */
-   	protected void init(String fileContents) {
-        findWordOccurrences(fileContents);
-        indexOccurrences(fileContents);
-   	}
 
 	/**
 	 * Initializes the text searcher with the contents of a text file.
@@ -171,19 +195,28 @@ public class TextSearcher {
 			w.write(buf,0,readCount);
 		}
 
-		inputText = w.toString();
-		init(inputText);
+		// Note:  If original API (e.g., with init(...)) method were required,
+        // these three fields and initializations could be moved to a
+        // subcomponent class:
+
+        indexedText = w.toString();
+        wordOccurrences = findWordOccurrences(indexedText);
+        occurrenceListsByWord = indexOccurrences(indexedText, wordOccurrences);
 	}
-	
+
+	// Note:  Possible optimization:  If client can accept CharSequence[]
+    //   instead of necessarily String[], return instances of an implementation
+    //   of CharSequence that indexes into indexedText, re-using its already
+    //   allocated character array, rather than making its own character array
+    //   to hold a copy of the substring of characterrs it presents (as
+    //   String.substring(...) does).)
+    //   - Saves array overhead and N characters for each hit result.
+    //   - Caveat:  Holding onto any such hit-result CharSequence object blocks
+    //     garbage-colllecting the possibly huge full-file-contents' String's
+    //     character array.
 
 
-	/*
-
-	      (inputText + ' ')   //????? try to fix end hack (avoid copying input)
-
-	 */
-
-	/**
+    /**
 	 * 
 	 * @param queryWord The word to search for in the file contents.
 	 * @param contextWords The number of words of context to provide on
@@ -191,59 +224,51 @@ public class TextSearcher {
 	 * @return One context string for each time the query word appears in the file.
 	 */
 	public String[] search(String queryWord, int contextWords) {
-        String canonicalQueryWord = canonicalizeWord(queryWord);
-        List<Occurrence> wordOccurrences =
+        final String canonicalQueryWord = canonicalizeWord(queryWord);
+        final List<Occurrence> queryWordOccurrences =
                 occurrenceListsByWord.getOrDefault(canonicalQueryWord,
                                                    Collections.emptyList());
+        // (Named values to try to make uses clearer.)
+        final int minOccIndex = 0;
+        final int maxOccIndex = wordOccurrences.size() - 1;  // (Indexed)
+        final int minTextCharOffset = 0;
+        final int maxTextCharOffset = indexedText.length();  // (Used as range)
 
-        List<String> nameThis1 =
-            wordOccurrences.stream().map(occurrence -> {
-                int occOffset = occurrence.occOffset;
-                System.err.println("occurrence #i: " + occurrence);
+        final List<String> resultsList =
+            queryWordOccurrences.stream().map(occurrence -> {
+                final int hitOccIndex = occurrence.getOccIndex();
 
                 final int contextStartCharOffset;
                 {
-                    int nominalContextStartOccurrenceOffset = occOffset - contextWords;
-                    if (nominalContextStartOccurrenceOffset < 0) {
-                        contextStartCharOffset = 0;
+                    final int startOccurrenceIndex = hitOccIndex - contextWords;
+                    if (startOccurrenceIndex < minOccIndex) {
+                        contextStartCharOffset = minTextCharOffset;
                     }
                     else {
-                        Occurrence startOccurrence = occurrences.get(nominalContextStartOccurrenceOffset);
-                        contextStartCharOffset = startOccurrence.startChOffset;
+                        Occurrence startOcc =
+                                wordOccurrences.get(startOccurrenceIndex);
+                        contextStartCharOffset = startOcc.getStartChOffset();
                     }
                 }
                 final int contextEndCharOffset;
                 {
-                    int nominalContextEndOccurrenceOffset = occOffset + contextWords;
-                    if (nominalContextEndOccurrenceOffset > occurrences.size() - 1) {
-                        contextEndCharOffset = inputText.length();
+                    final int endOccurrenceIndex = hitOccIndex + contextWords;
+                    if (endOccurrenceIndex > maxOccIndex) {
+                        contextEndCharOffset = maxTextCharOffset;
                     }
                     else {
-                        Occurrence endOccurrence = occurrences.get(nominalContextEndOccurrenceOffset);
-                        contextEndCharOffset = endOccurrence.endChOffset;
+                        Occurrence endOccurrence =
+                                wordOccurrences.get(endOccurrenceIndex);
+                        contextEndCharOffset = endOccurrence.getEndChOffset();
                     }
                 }
 
-                String resultString = inputText.substring(contextStartCharOffset, contextEndCharOffset);
+                String resultString = indexedText.substring(contextStartCharOffset,
+                                                            contextEndCharOffset);
                 return resultString;
 
             }).collect(Collectors.toList());
-
-        String[] nameThis2 = nameThis1.toArray(new String[0]);
-
-/*
-
-
-   	      val results = {
-   	        wordOccurrences.map(wordOccurrence => {
-   	          resultString
-   	        })
-   	      }
-   	      results
-   	    }
-
-*/
-        return nameThis2;
+        return resultsList.toArray(new String[0]);
 	}
 }
 
